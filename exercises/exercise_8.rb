efdbdf0fb4e05d049f2a4ff12a0d7be202d015d1
Brandon <bmacdonald1986@gmail.com>
2021-05-25 03:25:48 +0000

    Add Exercise 8

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,14 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+employee = @store1.employees.create(first_name: "Captain", last_name: "America", hourly_rate: 199)
+puts employee.password
